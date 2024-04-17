58f60cfa86e8ef14b1d23175331b6483779ea1ad
emhberg <114058725+emhberg@users.noreply.github.com>
2024-04-17 16:16:57 +0200

    commit

--- Pages/Knowledge base/ViewArticle.js
+++ Pages/Knowledge base/ViewArticle.js
@@ -1,35 +1,78 @@
-import { Link } from "@react-navigation/native";
-import { SafeAreaView, ScrollView, Text, View, Linking } from "react-native";
+import {Link} from '@react-navigation/native';
+import {SafeAreaView, ScrollView, Text, View, Linking} from 'react-native';
 import Markdown from 'react-native-markdown-display';
 
-export const ViewArticle = ({ route }) => {
+export const ViewArticle = ({route}) => {
+  const {article} = route.params;
 
-    const { article } = route.params;
+  const goToLink = () => {
+    Linking.openURL(article.get('website'));
+  };
 
-    const goToLink = () => {
-        Linking.openURL(article.get('website'));
-    };
+  return (
+    <SafeAreaView>
+      <ScrollView style={{padding: 20, marginBottom: 20}}>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            marginBottom: 15,
+          }}>
+          {article.get('title')}
+        </Text>
+        <Text
+          style={{
+            fontSize: 19,
+            fontStyle: 'italic',
+            textAlign: 'center',
+            marginBottom: 10,
+          }}>
+          {article.get('subHeader')}
+        </Text>
+        <View
+          style={{
+            borderWidth: 1,
+            backgroundColor: 'black',
+            marginVertical: 10,
+            width: 300,
+            alignSelf: 'center',
+          }}></View>
 
-    return (
-        <SafeAreaView>
-            <ScrollView style={{ padding: 20, marginBottom: 20 }}>
-                <Text style={{ fontSize: 32, fontWeight: 'bold', textAlign: 'center' }}>{article.get('title')}</Text>
-                <Text style={{ fontSize: 20, fontStyle: 'italic', textAlign: 'center' }}>{article.get('subHeader')}</Text>
-                <View style={{ borderWidth: 1, backgroundColor: 'black', marginVertical: 10, width: 300, alignSelf: 'center' }}></View>
-
-                <Markdown style={{ paragraph: { fontSize: 18 }, bullet_list: { fontSize: 18 }, heading3: { fontSize: 22, marginTop: 30 }, list_item: { marginVertical: 5 } }}>{article.get('text')}</Markdown>
-                <View style={{ marginVertical: 30 }}>
-                    <View style={{ borderWidth: 0.2, borderColor: 'grey', backgroundColor: 'grey', marginBottom: 30, width: 200, alignSelf: 'center' }}></View>
-                    <Text>Denne artikel er skrevet af
-                        <Text style={{ fontWeight: 'bold' }}> {article.get('author')}.</Text>
-                    </Text>
-                    <Text style={{ marginVertical: 10 }}>Hele artiklen samt mere info kan findes på
-                        <Text style={{ fontStyle: 'italic' }} onPress={goToLink}> {article.get('website')}</Text>
-                    </Text>
-                </View>
-            </ScrollView>
-        </SafeAreaView >
-    );
-}
+        <Markdown
+          style={{
+            paragraph: {fontSize: 18},
+            bullet_list: {fontSize: 18},
+            heading3: {fontSize: 22, marginTop: 30, fontWeight: 'bold'},
+            list_item: {marginVertical: 5},
+          }}>
+          {article.get('text')}
+        </Markdown>
+        <View style={{marginVertical: 30}}>
+          <View
+            style={{
+              borderWidth: 0.2,
+              borderColor: 'grey',
+              backgroundColor: 'grey',
+              marginBottom: 30,
+              width: 200,
+              alignSelf: 'center',
+            }}></View>
+          <Text>
+            Denne artikel er skrevet af
+            <Text style={{fontWeight: 'bold'}}> {article.get('author')}.</Text>
+          </Text>
+          <Text style={{marginVertical: 10}}>
+            Hele artiklen samt mere info kan findes på
+            <Text style={{fontStyle: 'italic'}} onPress={goToLink}>
+              {' '}
+              {article.get('website')}
+            </Text>
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default ViewArticle;
