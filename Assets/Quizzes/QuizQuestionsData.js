25357f89cbbc54f9a5236bed8aaf75487f88eeba
emhberg <114058725+emhberg@users.noreply.github.com>
2024-04-17 15:02:29 +0200

    Oversat til dansk

--- Assets/Quizzes/QuizQuestionsData.js
+++ Assets/Quizzes/QuizQuestionsData.js
@@ -1,56 +1,39 @@
 const data = {
-    Structure: {
-        1: [
-            {
-                question: "What should you do when approaching a yellow traffic light?",
-                options: [
-                    "Speed up and cross the intersection quickly",
-                    "Come to a complete stop",
-                    "Slow down and prepare to stop",
-                    "Ignore the light and continue driving",
-                ],
-                correct_option: "Slow down and prepare to stop",
-            },
-            {
-                question: "question 2",
-                options: [
-                    "Option 1",
-                    "Option 2",
-                    "Option 3",
-                    "Option 4",
-                ],
-                correct_option: "Option 2",
-            },
+  Structure: {
+    1: [
+      {
+        question: 'What should you do when approaching a yellow traffic light?',
+        options: [
+          'Speed up and cross the intersection quickly',
+          'Come to a complete stop',
+          'Slow down and prepare to stop',
+          'Ignore the light and continue driving',
         ],
-        2: [
-            {
-                question: "question 2",
-                options: [
-                    "Option 1",
-                    "Option 2",
-                    "Option 3",
-                    "Option 4",
-                ],
-                correct_option: "Option 2",
-            },
-        ],
-    },
-    overwhelmed: {
-        1: [
-            {
-                question: "question 2",
-                options: [
-                    "Option 1",
-                    "Option 2",
-                    "Option 3",
-                    "Option 4",
-                ],
-                correct_option: "Option 2",
-            },
-        ],
-
-    },
-
+        correct_option: 'Slow down and prepare to stop',
+      },
+      {
+        question: 'question 2',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        correct_option: 'Option 2',
+      },
+    ],
+    2: [
+      {
+        question: 'question 2',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        correct_option: 'Option 2',
+      },
+    ],
+  },
+  overwhelmed: {
+    1: [
+      {
+        question: 'question 2',
+        options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+        correct_option: 'Option 2',
+      },
+    ],
+  },
 };
 
 export default data;
