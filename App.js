4f30d822323133304795f208f9c0619ca034fe8d
Nikoline Vejby <nikolinevejby96@gmail.com>
2024-06-03 13:11:26 +0200

    Added firebase notifications

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { PermissionsAndroid, Alert } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SideMenu from './Navigation/SideMenu';
@@ -8,33 +9,27 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ThemeProvider, useThemeContext } from './Assets/Theme/ThemeContext';
 import { UserProvider } from './Components/UserContext';
 import { configurePushNotifications } from './Components/PushNotificationMethods';
+import messaging from '@react-native-firebase/messaging';
 
 
 Parse.setAsyncStorage(AsyncStorage);
 Parse.initialize('JgIXR8AGoB3f1NzklRf0k9IlIWLORS7EzWRsFIUb', 'NBIxAIeWCONMHjJRL96JpIFh9pRKzJgb6t4lQUJD');
 Parse.serverURL = 'https://parseapi.back4app.com/'
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 configurePushNotifications();
 
 function App() {
 
   const { theme } = useThemeContext();
-  const [ID, setID] = useState('');
-  //const [expoPushToken, setExpoPushToken] = useState('');
-  //const [notification, setNotification] = useState(false);
-  //const notificationListener = useRef();
-  //const responseListener = useRef();
 
   useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
 
-    //configurePushNotifications();
+    return unsubscribe;
   }, []);
 
-  async function themeQuery() {
-    let themeQ = new Parse.Query('Settings');
-    themeQ.contains('user', ID);
-    const Result = await themeQ.find();
-    const chosenTheme = (Result[0].get('theme'));
-  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1, }}>
@@ -43,13 +38,7 @@ function App() {
 
         <SafeAreaView style={{ flex: 1 }}>
           <SideMenu />
-          {/*<Button
-            title="Press to Send Notification"
-            onPress={async () => {
-              await sendPushNotification(expoPushToken);
-            }}
-          />*/}
-          {/* <BottomNavigation /> */}
+
         </SafeAreaView>
 
       </NavigationContainer>
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import 'react-native-gesture-handler'
+import messaging from '@react-native-firebase/messaging';
 import 'react-native-get-random-values';
 import 'expo-random';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { configurePushNotifications } from './Components/PushNotificationMethods';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
